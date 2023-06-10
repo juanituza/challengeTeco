9099c0675c53ee2c04ba12f693c76384c860b302
juanituza <juanituza85@gmail.com>
2023-06-10 14:28:44 -0300

    cambio auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,35 +1,15 @@
-export const privacy = (privacyType)=>{
-    return (req,res,next)=>{
-        const{user}= req.session;
+export const privacy = (privacyType) => {
+    return (req, res, next) => {
+        const { user } = req.session;
         switch (privacyType) {
             case "PRIVATE":
                 //Esta ruta es para dejar pasar a los que se hayan logueado
                 if (user) next();
-                else res.redirect('/login');                
+                else res.redirect('/login');
                 break;
             case "NO_AUTHENTICATED":
-                if(!user) next();
+                if (!user) next();
                 else res.redirect('/profile')
-                
-        
-            
         }
     };
 };
-
-
-// export const privacy = (privacyType) => {
-//     return (req, res, next) => {
-//         const { user } = req.session;
-//         switch (privacyType) {
-//             case "PRIVATE":
-//                 //Esta validación es para dejar pasar a los que se hayan logueado.
-//                 if (user) next();
-//                 else res.redirect('/login')
-//                 break;
-//             case "NO_AUTHENTICATED":
-//                 if (!user) next()
-//                 else res.redirect('/profile')
-//         }
-//     };
-// };
